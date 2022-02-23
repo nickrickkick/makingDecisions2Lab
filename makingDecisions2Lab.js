47c2690939b5147dabab766d17a258b196fe9f2c
Michael Nowlin <michaelnowlin5@gmail.com>
2022-02-22 20:29:10 -0700

    checking problem 5 with giving me the right letter grade for 74 which should be a c

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -56,7 +56,7 @@ for (let i = 0; i < nums.length; i++){
     evensArr.push(nums[i]);
   }
 }
-console.log(evensArr);
+
 
 
 
@@ -71,6 +71,17 @@ var score = 74
 */
 
 //Code Here
+if (score >= 90){
+  console.log("A");
+} else if (score >= 80 && score <= 89){
+  console.log("B");
+} else if (score >= 70 && score <= 79){
+  console.log("C");
+} else if (score >= 60 && score <= 69){
+  console.log("D");
+} else {
+  console.log("F");
+}
 
 
 ////////// Intermediate Problems //////////
